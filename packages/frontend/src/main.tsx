39fce54570f1a25d90593d380ddb0981a52747d5
Priyansh Yadav <priyanshy0219@gmail.com>
2025-09-21 18:37:16 +0530

    Added React query dev tools

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import AuthProvider from './providers/AuthProvider';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { CookiesProvider } from 'react-cookie';
+import AuthProvider from "./providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CookiesProvider } from "react-cookie";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -22,14 +23,15 @@ declare module "@tanstack/react-router" {
   }
 }
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <CookiesProvider>
         <AuthProvider>
           <RouterProvider router={router} />
+          <ReactQueryDevtools />
         </AuthProvider>
       </CookiesProvider>
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
